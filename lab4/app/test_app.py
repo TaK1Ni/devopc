061230f2551a276de1fa91b13623179ac9185589
TaK1Ni <pnv0610@mail.ru>
2024-12-25 22:31:23 +0300

    fixtest

--- lab4/app/test_app.py
+++ lab4/app/test_app.py
@@ -4,18 +4,19 @@ from flask_login import login_user
 from app import app, db
 import mysql.connector
 
+import pytest
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import app, db  # Убедитесь, что db - это экземпляр SQLAlchemy
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     app.config['WTF_CSRF_ENABLED'] = False 
 
-    connection = mysql.connector.connect(
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        host=app.config['MYSQL_HOST'],
-        database=app.config['MYSQL_DATABASE']
-    )
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://test:test@192.168.1.4/test'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
     with app.test_client() as client:
         yield client
 
