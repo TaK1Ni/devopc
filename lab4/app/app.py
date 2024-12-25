66ebd78808132be4be4dba3af65624511c423369
TaK1Ni <57948474+TaK1Ni@users.noreply.github.com>
2024-12-25 21:12:38 +0300

    Update app

--- lab4/app/app.py
+++ lab4/app/app.py
@@ -116,7 +116,7 @@ def login():
                     login_user(User(user.id, user.login), remember=check)
                     next_url = request.args.get('next') or url_for('index')
                     flash('Вы успешно вошли!', 'success')
-                    return redirect(next_url)
+                    return make_response(203)
                 else:
                     flash('Неверные учетные данные.', 'danger')
         except mysql.connector.errors.DatabaseError:
